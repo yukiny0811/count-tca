e9f7881dab40f495a713f4edc243bf35a8464fd6
yukiny0811 <yukiny0811@gmail.com>
2022-09-07 01:44:20 +0900

    feat: count

---
+++ Count/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  Count
+//
+//  Created by クワシマ・ユウキ on 2022/09/07.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct CountState: Equatable {
+    var number: Int = 0
+}
+
+enum CountAction {
+    case add
+    case minus
+    case multiply2
+    case divide2
+    case clear
+}
+
+struct CountEnvironment {
+    
+}
+
+final class CountReducer {
+    static let countStore: Store<CountState, CountAction> = Store(initialState: CountState(), reducer: Reducer<CountState, CountAction, CountEnvironment>{ state, action, env in
+        switch action {
+        case .add:
+            state.number += 1
+        case .minus:
+            state.number -= 1
+        case .multiply2:
+            state.number *= 2
+        case .divide2:
+            state.number /= 2
+        case .clear:
+            state.number = 0
+        }
+        return .none
+    }, environment: CountEnvironment())
+}
+
+struct ContentView: View {
+    var body: some View {
+        WithViewStore(CountReducer.countStore) { countViewStore in
+            VStack {
+                Text(String(countViewStore.number))
+                    .padding()
+                    .font(.title)
+                HStack {
+                    Button("plus") {
+                        countViewStore.send(.add)
+                    }.padding()
+                    
+                    Button("minus") {
+                        countViewStore.send(.minus)
+                    }.padding()
+                }
+                HStack {
+                    Button("multiply2") {
+                        countViewStore.send(.multiply2)
+                    }.padding()
+                    Button("divide2") {
+                        countViewStore.send(.divide2)
+                    }.padding()
+                }
+                Button("clear") {
+                    countViewStore.send(.clear)
+                }.padding()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Count/CountApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountApp.swift
+//  Count
+//
+//  Created by クワシマ・ユウキ on 2022/09/07.
+//
+
+import SwiftUI
+
+@main
+struct CountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
